8abab42499f7c7b769a7d60c916ee79c9c2988b3
Uchchash346 <mru416@gmail.com>
2022-01-27 14:52:26 +0600

    Navbar setup

--- src/components/DesktopView/DeskNavBar.js
+++ src/components/DesktopView/DeskNavBar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import "./DeskNavBar.css"
+import { AiOutlineMenu, AiOutlineHome, AiOutlineMessage } from "react-icons/ai";
+import { FiSettings } from "react-icons/fi";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+
 
 const DeskNavBar = () => {
     return (
         <div>
-            <h1>This is desktop navbar</h1>
-            <h2>Hurray</h2>
+            <div class="wrapper">
+                <div class="sidebar">
+                    <h2><AiOutlineMenu class="text-white" /></h2>
+                    <ul>
+                        <li><a href="#"><AiOutlineHome class="react-icon-area text-white" /></a></li>
+                        <li><a href="#"><AiOutlineMessage class="react-icon-area text-white" /></a></li>
+                        <li><a href="#"><FiSettings class="react-icon-area text-white" /></a></li>
+                        <li><a href="#" id="react-logout-icon"><RiLogoutBoxRLine class="react-icon-area text-white" /></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
